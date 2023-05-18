6af7076cc8b08d0f2c1656d15ba2765b4b23eaee
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-18 19:23:10 +0200

    routing, login page fetch login, Auth login, packages

--- GeoProfs/src/main.jsx
+++ GeoProfs/src/main.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
-
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { render } from "react-dom";
+import { SnackbarProvider } from "notistack";
+import App from "./App";
 import Dashboard from "./dashboard";
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "*",
+    element: "not found",
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route exact path="/Dashboard">
-          <Dashboard />
-        </Route>
-        <Route exact path="*">
-          <p>not found</p>
-        </Route>
-      </Switch>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <SnackbarProvider>
+      <RouterProvider router={router} />
+    </SnackbarProvider>
+  </React.StrictMode>
 );
