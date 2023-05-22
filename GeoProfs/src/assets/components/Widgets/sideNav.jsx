5f1e1f10198a0ddd1f085792ad9d52fa48991338
TijnKlabbers <Tijnklabbers@gmail.com>
2023-05-22 12:56:29 +0200

    :

---
+++ GeoProfs/src/assets/components/Widgets/sideNav.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faSignal,
+  faUserXmark,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import profilePicture from "../../../assets/profile.jpg";
+import logo from "../../../assets/logoGeoProf.png";
+const Sidebar = () => {
+  return (
+    <div className="sidenav">
+      <img className="logo" src={logo} alt="" />
+      <div className="lowerBlock">
+        <div className="infoBlock">
+          <img className="profilePicture" src={profilePicture} alt="" />
+          <p className="name">Bart Janssen</p>
+          <p className="function">functie</p>
+        </div>
+        <div className="categoryBlock">
+          <a className="active">
+            <FontAwesomeIcon className="home" icon={faHouse} />
+            <p>Home</p>
+          </a>
+          <a className="nav_icons">
+            <FontAwesomeIcon icon={faSignal} />
+            Status
+          </a>
+          <a className="nav_icons">
+            <FontAwesomeIcon icon={faUserXmark} />
+            Days off
+          </a>
+        </div>
+        <div>
+          <a className="logOut nav_icons" href="/logout">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            Log out
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- GeoProfs/src/dashboard.jsx
+++ GeoProfs/src/dashboard.jsx
@@ -1,54 +1,17 @@
 import "./dashboard.css";
-import profilePicture from "../src/assets/profile.jpg";
-import logo from "../src/assets/logoGeoProf.png";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Status from "./assets/components/Widgets/statusComponent";
 import VerlofComponent from "./assets/components/Widgets/request";
 import DaysOff from "../src/assets/components/Widgets/daysOff";
-import arrow from "../src/assets/arrow.png";
-import {
-  faHouse,
-  faSignal,
-  faUserXmark,
-  faRightFromBracket,
-  faCalendarDays,
-  faChevronRight,
-  faUsers,
-} from "@fortawesome/free-solid-svg-icons";
+import ColleagueDayOff from "./assets/components/Widgets/ColleaguesDaysOff";
+import SideNav from "./assets/components/Widgets/sideNav";
+import { faChevronRight, faUsers } from "@fortawesome/free-solid-svg-icons";
 
 function Dashboard() {
   return (
     <div className="background">
-      <div className="sidenav">
-        <img className="logo" src={logo} alt="" />
-        <div className="lowerBlock">
-          <div className="infoBlock">
-            <img className="profilePicture" src={profilePicture} alt="" />
-            <p className="name">Bart Janssen</p>
-            <p className="function">functie</p>
-          </div>
-          <div className="categoryBlock">
-            <a className="active">
-              <FontAwesomeIcon className="home" icon={faHouse} />
-              <p>Home</p>
-            </a>
-            <a className="nav_icons">
-              <FontAwesomeIcon icon={faSignal} />
-              Status
-            </a>
-            <a className="nav_icons">
-              <FontAwesomeIcon icon={faUserXmark} />
-              Days off
-            </a>
-          </div>
-          <div>
-            <a className="logOut nav_icons" href="/logout">
-              <FontAwesomeIcon icon={faRightFromBracket} />
-              Log out
-            </a>
-          </div>
-        </div>
-      </div>
+      <SideNav />
       <div className="componentsBlock">
         <div className="left">
           <div className="Block">
@@ -77,28 +40,13 @@ function Dashboard() {
             </div>
             <div className="line"></div>
             <div className="colleaguesDaysOffBlock_block">
-              <div className="colleagueDayOffBlock">
-                <div className="colleagueDayOffBlock_left">
-                  <img className="logo_colleague" src={profilePicture} alt="" />
-                  <div>
-                    <p className="colleague_name">Rick Rietbergen</p>
-                    <p className="colleague_function">Development</p>
-                  </div>
-                </div>
-                <div className="colleagueDayOffBlock_right">
-                  <div className="align-center">
-                    <div className="beginMonth">DEC</div>
-                    <div className="day">16</div>
-                    <div className="endMonth">TUE</div>
-                  </div>
-                  <img className="arrow" src={arrow}></img>
-                  <div className="align-center">
-                    <div className="beginMonth">DEC</div>
-                    <div className="day">30</div>
-                    <div className="endMonth">DEC</div>
-                  </div>
-                </div>
-              </div>
+              <ColleagueDayOff />
+              <ColleagueDayOff />
+              <ColleagueDayOff />
+              <ColleagueDayOff />
+              <ColleagueDayOff />
+              <ColleagueDayOff />
+              <ColleagueDayOff />
             </div>
           </div>
         </div>
