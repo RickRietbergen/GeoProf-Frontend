06b1c0cee72b7abb973cbe30baa6cd1924811266
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-31 12:16:35 +0200

    fetch get method

--- GeoProfs/src/assets/components/Widgets/statusComponent.jsx
+++ GeoProfs/src/assets/components/Widgets/statusComponent.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ConfirmedStatus from "../status/Confirmed";
 import DeniedStatus from "../status/Denied";
 import PendingStatus from "../status/Pending";
+import useAuth from "../Hooks/useAuth";
 
 const StatusComponent = () => {
+  const { isLoggedIn, authFetch } = useAuth();
+  const [verlofData, setVerlofData] = useState(null);
+
+  const fetchVerlof = () => {
+    authFetch("GetVerlof", { Method: "GET" })
+    .then((r) => r.json())
+    .then((data) => setVerlofData(data));
+  }
+  console.log(verlofData);
+
+  useEffect(() => {
+    if (!isLoggedIn) navigate("/login");
+
+    fetchVerlof();
+  }, []);
+  
   return (
     <div className="statusComponent">
       <DaysOffTop />
