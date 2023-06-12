25ce7f81411fd725ffac31ffcaf548fed1f2b8fe
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-12 22:39:59 +0200

    remove unnecessary imports

--- GeoProfs/src/dashboard.jsx
+++ GeoProfs/src/dashboard.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import "./dashboard.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import Status from "./assets/components/Widgets/statusComponent";
-// import VerlofComponent from "./assets/components/Widgets/request";
-// import DaysOff from "../src/assets/components/Widgets/daysOff";
-// import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
-// import ColleagueDayOff from "../src/assets/components/Widgets/ColleaguesDaysOff.jsx";
 import SideNav from "./assets/components/Widgets/sideNav";
 import Smallernav from "./assets/components/Widgets/smallerSideNav";
 import ManagerDashboard from "../src/assets/components/Manager/ManagerDashboard";
