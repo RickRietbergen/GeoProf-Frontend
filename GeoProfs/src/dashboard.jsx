88f88ae973465c07e351871fa60c34bc86af74e6
TijnKlabbers <Tijnklabbers@gmail.com>
2023-06-12 21:02:22 +0200

    making a component from the manager code

--- GeoProfs/src/dashboard.jsx
+++ GeoProfs/src/dashboard.jsx
@@ -2,65 +2,15 @@ import React from "react";
 import "./dashboard.css";
 import SideNav from "./assets/components/Widgets/sideNav";
 import Smallernav from "./assets/components/Widgets/smallerSideNav";
-import UserDashboard from "./assets/components/userDashboard";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ColleagueDayOff from "./assets/components/Widgets/ColleaguesDaysOff";
-import RequestBlock from "./assets/components/Widgets/requestBlock";
-import {
-  faUsers,
-  faClock,
-  faEnvelope,
-  faCheck,
-  faXmark,
-  faEllipsis,
-} from "@fortawesome/free-solid-svg-icons";
-
+import ManagerDashboard from "../src/assets/components/Manager/ManagerDashboard";
+import UserDashboard from "../src/assets/components/userDashboard";
 function Dashboard() {
   return (
     <div className="background">
       <SideNav value="dashboard" className="nav" />
       <Smallernav className="smallNav" />
-      {/* <UserDashboard /> */}
-      <div className="componentsBlock componentsBlock_manager">
-        <div className="manager_block_above">
-          <div className="managers_blocks">
-            <div className="managers_blocks_title">
-              <FontAwesomeIcon icon={faUsers} />
-              <p> Employees</p>
-            </div>
-            <div className="managers_blocks_number">20</div>
-          </div>
-          <div className="managers_blocks">
-            <div className="managers_blocks_title">
-              <FontAwesomeIcon icon={faClock} />
-              <p> To Request</p>
-            </div>
-            <div className="managers_blocks_number">20</div>
-          </div>
-          <div className="managers_blocks">
-            <div className="managers_blocks_title">
-              <FontAwesomeIcon icon={faEnvelope} />
-              <p>Notifications</p>
-            </div>
-            <div className="managers_blocks_number">20</div>
-          </div>
-        </div>
-
-        <div className="manager_block_bottom">
-          <div className="titleName">
-            Recent Requests
-            <button className="dotsMore">
-              <FontAwesomeIcon icon={faEllipsis} />
-            </button>
-          </div>
-          <div className="requestBlocks">
-            <RequestBlock />
-            <RequestBlock />
-            <RequestBlock />
-            <RequestBlock />
-          </div>
-        </div>
-      </div>
+      <UserDashboard />
+      {/* <ManagerDashboard /> */}
     </div>
   );
 }
